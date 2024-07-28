99a879f07a936749419682cc83d0a57b8cfd4407
Md Shoaib Akhtar <azaan3842@gmail.com>
2024-07-28 20:15:08 +0530

    Added multiple component

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,83 @@
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import Promotion from './Promotion'
+import Pricing from './Pricing'
+import Statistics from './Statistics'
+import Newsletter from './Newsletter';
+import Jobcard from './Jobcard';
+import Collab from './Collab';
+import Banner from './Banner';
+import Form from './Form';
+import Team from './Team';
+import Image from "next/image";
+import Testimonials from './Testimonials';
+import img from "../../dashboard_image_1.jpg"
+const features = [
+    {
+        name: 'Push to deploy.',
+        description:
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+        icon: CloudArrowUpIcon,
+    },
+    {
+        name: 'SSL certificates.',
+        description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+        icon: LockClosedIcon,
+    },
+    {
+        name: 'Database backups.',
+        description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+        icon: ServerIcon,
+    },
+]
+
+export default function Dashboard() {
+    return (
+        <div>
+            <div className="overflow-hidden bg-white py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                        <div className="lg:pr-8 lg:pt-4">
+                            <div className="lg:max-w-lg">
+                                <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+                                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+                                <p className="mt-6 text-lg leading-8 text-gray-600">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                                    iste dolor cupiditate blanditiis ratione.
+                                </p>
+                                <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                                    {features.map((feature) => (
+                                        <div key={feature.name} className="relative pl-9">
+                                            <dt className="inline font-semibold text-gray-900">
+                                                <feature.icon aria-hidden="true" className="absolute left-1 top-1 h-5 w-5 text-indigo-600" />
+                                                {feature.name}
+                                            </dt>{' '}
+                                            <dd className="inline">{feature.description}</dd>
+                                        </div>
+                                    ))}
+                                </dl>
+                            </div>
+                        </div>
+                        <Image
+                            alt="Product screenshot"
+                            src={img}
+                            width={2432}
+                            height={1442}
+                            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+                        />
+                    </div>
+                </div>
+            </div>
+            <Promotion />
+            <Pricing />
+            <Statistics />
+            <Newsletter />
+            <Jobcard />
+            <Collab />
+            <Banner />
+            <Form />
+            <Team />
+            <Testimonials />
+        </div>
+
+    )
+}
