59e82e11476f698aec2dbc722b7dfe805cb6d64a
Md Shoaib Akhtar <azaan3842@gmail.com>
2024-07-29 22:10:52 +0530

    Added new navbar

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -33,7 +33,10 @@ const features = [
 export default function Dashboard() {
     return (
         <div>
-            <div className="overflow-hidden bg-white py-24 sm:py-32">
+            <div className="pt-16">
+            <Banner />
+        </div>
+            <div className="overflow-hidden bg-white py-12 sm:py-12">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                         <div className="lg:pr-8 lg:pt-4">
@@ -73,7 +76,6 @@ export default function Dashboard() {
             <Newsletter />
             <Jobcard />
             <Collab />
-            <Banner />
             <Form />
             <Team />
             <Testimonials />
