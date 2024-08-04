6a122b24ce18a27dfbf4cbf3728a76d7e405e26e
Md Shoaib Akhtar <azaan3842@gmail.com>
2024-08-04 16:43:14 +0530

    Make responsive

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -7,20 +7,20 @@ import Footer from '../../components/Footer';
 export default function jobs() {
     return (
         <div className="max-h-[85vh]">
-        <div className="w-full grid p-12 px-16 grid-cols-[38%_minmax(50%,_1fr)] gap-8">
-            <div className="max-h-[85vh] overflow-scroll no-scrollbar">
-                <Jobcard/>
-                <Jobcard/>
-                <Jobcard/>
-                <Jobcard/>
-                <Jobcard/>
-                <Jobcard/>
+            <div className="w-full grid md:flex-row p-4 sm:p-8 md:px-16 lg:p-10 md:grid-cols-[38%_minmax(50%,_1fr)] lg:grid-cols-[38%_minmax(50%,_1fr)] xl:grid-cols-[38%_minmax(50%,_1fr)] gap-8">
+                <div className="max-h-[85vh] overflow-scroll no-scrollbar">
+                    <Jobcard />
+                    <Jobcard />
+                    <Jobcard />
+                    <Jobcard />
+                    <Jobcard />
+                    <Jobcard />
+                </div>
+                <div className="max-h-[85vh] overflow-scroll no-scrollbar sm:none md:none lg:none xl:none">
+                    <JobDetails />
+                </div>
             </div>
-            <div className="max-h-[85vh] overflow-scroll no-scrollbar">
-                <JobDetails />
-            </div>
-        </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
