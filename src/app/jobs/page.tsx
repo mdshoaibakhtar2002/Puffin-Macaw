e87264ddf261b908ab7a5291b50beffa07517620
Md Shoaib Akhtar <azaan3842@gmail.com>
2024-08-10 19:07:23 +0530

    Dynamic job card data

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -38,11 +38,6 @@ export default function jobs() {
             <div className="w-full grid md:flex-row p-4 sm:p-8 md:px-16 lg:p-10 md:grid-cols-[38%_minmax(50%,_1fr)] lg:grid-cols-[38%_minmax(50%,_1fr)] xl:grid-cols-[38%_minmax(50%,_1fr)] gap-8">
                 <div className="max-h-[85vh] overflow-scroll no-scrollbar">
                     <Jobcard />
-                    <Jobcard />
-                    <Jobcard />
-                    <Jobcard />
-                    <Jobcard />
-                    <Jobcard />
                 </div>
                 <div className="max-h-[85vh] overflow-scroll no-scrollbar sm:none md:none lg:none xl:none">
                     <JobDetails />
